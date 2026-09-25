45ef3897aff2c2c1507fb06964bc68b74ca1007a
andlschs94-star <andlschs94@gmail.com>
2026-09-25 13:28:34 +0900

    Bump sync userscript to v1.0.1 with auto update URL

--- blablalink-sync.user.js
+++ blablalink-sync.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         NIKKE Gear Manager - BlablaLink 자동 장비 동기화
 // @namespace    https://andlschs94-star.github.io/nikke-/
-// @version      1.0.0
+// @version      1.0.1
+// @updateURL    https://raw.githubusercontent.com/andlschs94-star/nikke-/main/blablalink-sync.user.js
+// @downloadURL  https://raw.githubusercontent.com/andlschs94-star/nikke-/main/blablalink-sync.user.js
 // @description  로그인된 BlablaLink 세션에서 NIKKE 캐릭터별 기업장비 현황을 NIKKE Gear Manager로 전송합니다.
 // @author       NIKKE Gear Manager
 // @match        https://*.blablalink.com/*
